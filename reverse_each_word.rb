7b3dd5847c93b934d933a6b0676a33916d641ead
Nick Corona <coronaisme@gmail.com>
2019-12-05 00:16:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = []
+  split_string = string.split(" ")
+  split_string.collect do |string|
+    array.push(string.reverse)
+  end
+  array.join(" ")
+end
